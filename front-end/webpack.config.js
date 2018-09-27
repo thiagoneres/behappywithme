550503aab0e92e0f9a53b7e59d12de8fd6125e73
Thiago Neres <thiago.tnsb@gmail.com>
2018-09-27 08:33:16 -0300

    Installedd and configured the dependencies. Next tape: Configure Webpack,

---
+++ front-end/webpack.config.js
@@ -0,0 +1,8 @@
+const webpack = require('webpack'); //importando dependências
+const path = require('path');
+
+module.exports	=	{ //aqui serão feitas todas as configs do arquivo
+    
+    entry:	path.join(__dirname,	'src/index.jsx') //informa o arquivo principal da aplicação
+
+};
